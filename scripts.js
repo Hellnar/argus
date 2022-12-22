dd16af237657eb0bce4ed3f1ef072d864e13913d
StasKlymenko <Stas Klymenko>
2022-12-22 20:36:23 +0200

    Final update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getFirestore, collection, getDocs, updateDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getFirestore, collection, getDocs, updateDoc, doc, query, where, writeBatch } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js"
+import * as EMAILS from './testmails.json' assert { type: "json" }
 const firebaseConfig = {
     apiKey: "AIzaSyCOQhsXWANNs3Shl_HmNzF0SeWXTpAH5EI",
     authDomain: "argus-1a2aa.firebaseapp.com",
@@ -7,40 +8,70 @@ const firebaseConfig = {
     storageBucket: "argus-1a2aa.appspot.com",
     messagingSenderId: "628663199277",
     appId: "1:628663199277:web:d5ac86c6bc2491ec5fbaa4"
-};
-initializeApp(firebaseConfig);
+}
+let USER_ID = ""
+
+initializeApp(firebaseConfig)
 const db = getFirestore()
 handleBallClick()
 handleCloseModal()
+fillFirebase()
 
-// initPrize()
+
+// ============== DB REFS ====================
+
 const prizesRef = doc(db, "prizes", "AcPRU8K5FoFnxiD1fMLQ")
 const usersRef = collection(db, "users")
 
-async function initPrize() {
-    const prize = await choosePrize()
+
+
+// ============== GIVE PRIZES ====================
+
+async function initPrize(user) {
+    const prize = await choosePrize(user)
     document.querySelector(".prize-text").innerText = `Ура! ${prize}`
 }
 
-async function choosePrize() {
+async function choosePrize(user) {
     const prizes = await getPrizes()
     const randomInfinitePrize = randomFromArray(prizes.infinite)
+    let prize = ""
     if(prizes.jewelry === 0 && prizes.chocolate === 0) {
+        document.querySelector(".prize-phone").style.display = "none"
+        document.querySelector(".user-phone").style.display = "none"
+        document.querySelector(".take-prize").style.display = "block"
+        prize = randomInfinitePrize
+        await updateUser(user, prize)
         return `Вы выиграли ${randomInfinitePrize}!`
     }
     const randomNum = random(1, 7)
     if(prizes.jewelry > 0) {
         if(randomNum === 5) {
             updateDoc(prizesRef, {jewelry: prizes.jewelry - 1})
-            return `Вы выиграли украшение!`
+            document.querySelector(".prize-phone").style.display = "block"
+            document.querySelector(".user-phone").style.display = "block"
+            document.querySelector(".take-prize").style.display = "none"
+            prize = "украшение"
+            await updateUser(user, prize)
+            return `Вы выиграли украшение и скидку 15%!`
         }
     }
     if(prizes.chocolate > 0) {
         if(randomNum === 6) {
             updateDoc(prizesRef, {chocolate: prizes.chocolate - 1})
-            return `Вы выиграли шоколадку!`
+            document.querySelector(".prize-phone").style.display = "block"
+            document.querySelector(".user-phone").style.display = "block"
+            document.querySelector(".take-prize").style.display = "none"
+            prize = "шоколадку"
+            await updateUser(user, prize)
+            return `Вы выиграли шоколадку и скидку 15%!`
         }
     }
+    document.querySelector(".prize-phone").style.display = "none"
+    document.querySelector(".user-phone").style.display = "none"
+    document.querySelector(".take-prize").style.display = "block"
+    prize = randomInfinitePrize
+    await updateUser(user, prize)
     return `Вы выиграли ${randomInfinitePrize}!`
 }
 
@@ -49,13 +80,17 @@ async function getPrizes() {
     return response.docs[0].data()
 }
 
+async function updateUser(user, prize) {
+    const userRef = doc(db, "users", USER_ID)
+    updateDoc(userRef, {prize: prize, active: false})
+}
 
 
-// SEND EMAIL
+
+// ============== CHECK EMAIL ====================
 
 function submitEmail() {
     document.querySelector(".enter-email").addEventListener("submit", async (e) => {
-        console.log(`submit clicked`)
         e.preventDefault()
         const user = await checkEmail(e.target[0].value)
         if(user === null) {
@@ -63,8 +98,17 @@ function submitEmail() {
         } else {
             if(!user.active) {
                 document.querySelector(".prize-text").innerText = `Поздравляем! Вы уже выиграли ${user.prize}!`
+                if(user.prize === "шоколадку" || user.prize === "украшение") {
+                    document.querySelector(".prize-phone").style.display = "block"
+                    document.querySelector(".user-phone").style.display = "block"
+                    document.querySelector(".take-prize").style.display = "none"
+                } else {
+                    document.querySelector(".prize-phone").style.display = "none"
+                    document.querySelector(".user-phone").style.display = "none"
+                    document.querySelector(".take-prize").style.display = "block"
+                }
             } else {
-                initPrize()
+                initPrize(user)
             }
             document.querySelector(".enter-email").style.display = "none"
             document.querySelector(".modal-body").style.display = "flex"
@@ -73,14 +117,14 @@ function submitEmail() {
 }
 
 async function checkEmail(email) {
-    console.log(`checking ${email}`)
     const findEmail = await query(usersRef, where("email", "==", email))
     const user = await getDocs(findEmail)
+    if(user.docs.length <= 0) return null
+    USER_ID = user.docs[0].id
     return user.docs.length > 0 ? user.docs[0].data() : null
 }
 
 function userDoesntExist() {
-    console.log(`user doesn't exist`)
     document.querySelector(".email-error").style.display = "block"
 }
 
@@ -92,8 +136,8 @@ function handleBallClick() {
     document.querySelectorAll(".ball").forEach(ball => {
         ball.addEventListener("click", () => {
             resetModal()
-            document.querySelector(".modal-box").style.display = "flex"
             submitEmail()
+            document.querySelector(".modal-box").style.display = "flex"
         })
     })
 }
@@ -103,7 +147,7 @@ function resetModal() {
     document.querySelector(".modal-body").style.display = "none"
     document.querySelector(".enter-email input").value = ""
     document.querySelector(".enter-email input").value = ""
-    document.querySelector("#phone").value = ""
+    // document.querySelector("#phone").value = ""
 }
 
 function handleCloseModal() {
@@ -122,4 +166,20 @@ function random(min, max) {
 
 function randomFromArray(items) {
     return items[Math.floor(Math.random()*items.length)]
+}
+
+function fillFirebase() {
+    const users = EMAILS.default.map(email => {
+        return {
+            email: email,
+            active: true,
+            prize: ""
+        }
+    })
+    const batch = writeBatch(db)
+    users.forEach((user) => {
+        const docRef = doc(collection(db, "users"))
+        batch.set(docRef, user);
+    })
+    batch.commit()
 }
